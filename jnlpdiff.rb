3d2873d801a7d1b9dbc5039d4154a85302d60779
Scott Cytacki <scytacki@concord.org>
2012-11-29 10:26:55 -0500

    major refactoring of jnlpdiff
    This should make it a little easier to maintain.

--- jnlpdiff.rb
+++ jnlpdiff.rb
@@ -3,14 +3,137 @@
 require 'trollop'
 require_relative 'lib/jnlp'
 
-options = Trollop::options do
+$options = Trollop::options do
   opt :sizes, "Show sizes of difference"
   opt :classes, "Expand jars and compare classes"
   opt :show_content_diff, "Show which classes differ"
   opt :source_changes, "Look for pom inside of jar and lookup date of last source code change"
 end
 
-def print_differences(map, sizes, content_differences, options)
+class JarPair
+  attr_accessor :new_jar
+  attr_accessor :old_jar
+  attr_accessor :content_differences
+
+  def diff_versions?
+    old_jar && new_jar && new_jar.version_str != old_jar.version_str
+  end
+
+  def compaired_content?
+    @compaired_content
+  end
+
+  def diff_contents?
+    return false unless diff_versions?
+    if compaired_content?
+      return content_differences != ""
+    end
+
+    `rm -rf unzipped/old/*`
+    `rm -rf unzipped/new/*`
+    `unzip -d unzipped/old #{old_jar.local_path} -x META-INF/*`
+    `unzip -d unzipped/new #{new_jar.local_path} -x META-INF/*`
+    differences = `cd unzipped; diff -r old new`
+    if $?.to_i == 0
+      # the class files match in these jars
+      self.content_differences = ""
+    else
+      self.content_differences = differences
+    end
+    @compaired_content = true
+
+    return content_differences != ""
+  end
+
+  def different?
+    if options[:classes]
+      diff_contents?
+    else
+      diff_versions?
+    end
+  end
+
+  def source_change_date
+    return @source_change_date if @source_change_date
+
+    `rm -f tmp/*`
+    `unzip -j -d tmp #{nj.local_path} *pom.xml`
+    `cd tmp; mvn help:effective-pom -Doutput=effective-pom.xml`
+    # pull out the project/scm/connection url from the effective-pom.xml
+    scm_connection = `xpath tmp/effective-pom.xml "/project/scm/connection/text()"  2> /dev/null`
+    if scm_connection =~ /scm:svn:(.*)/
+      svn_info = `svn info #{$1}`
+      @source_change_date = svn_info.scan(/Last Changed Date:(.*)/)[0][0]
+    else
+      @source_change_date = "unknown date"
+    end
+  end
+
+  def new_jar_size
+    return @new_jar_size if @new_jar_size
+    return 0 if new_jar.nil?
+
+    begin
+      url = URI.parse(new_jar.url_pack_gz)
+      connection = Net::HTTP.new(url.host, url.port)
+      response = connection.request_head(url.path)
+      @new_jar_size = response.content_length
+    rescue
+      puts "failed to get jar #{url.path.to_s}"
+    end
+
+    @new_jar_size
+  end
+
+  def href
+    return new_jar.href if new_jar
+    return old_jar.href
+  end
+
+  def print_diff
+    items = []
+    if new_jar.nil?
+      items << "remove"
+    elsif old_jar.nil?
+      items << "add   "
+    elsif diff_versions?
+      # we still print differences even if the contents are the same
+      items << "change"
+    else
+      # no change
+      return
+    end
+
+    if $options[:classes]
+      # add a flag to indicate that only the versions are different
+      items << ((diff_versions? && !diff_contents?) ? "v" : " ")
+    end
+    items << "#{href}"
+    if $options[:sizes] && new_jar
+      items << "#{new_jar_size} bytes"
+    end
+
+    if old_jar
+      items << old_jar.version_str
+    end
+
+    if new_jar
+      items << new_jar.version_str
+    end
+
+    if $options[:source_changes] && different?
+      # only figure out the source change date if the jars are different because it is expensive
+      # and here we use the different? method so if the content has been compaired we only care about
+      # jars with different content
+      items << "#{source_changes_date}"
+    end
+
+    puts items.join("\t")
+
+    if $options[:show_content_diff] && diff_contents?
+      puts content_differences
+    end
+  end
 end
 
 puts "comparing #{ARGV[0]} to #{ARGV[1]}"
@@ -20,114 +143,57 @@ new_jnlp = Jnlp::Jnlp.new(ARGV[1])
 new_jars = new_jnlp.jars
 old_jars = old_jnlp.jars
 
-new_jar_map = {}
-new_jars.each{|nj|
-  new_jar_map[nj] = old_jars.find{|oj| oj.href == nj.href}
+pairs = new_jars.map{|nj|
+  pair = JarPair.new
+  pair.new_jar = nj
+  pair.old_jar = old_jars.find{|oj| oj.href == nj.href}
+  pair
 }
 
-diff_jars_map = {}
-new_jar_map.each{|nj,oj|
-  if oj.nil? || nj.version_str != oj.version_str
-    diff_jars_map[nj] = oj
+# add jars that are only in the old_jnlp
+old_jars.each{|oj|
+  unless pairs.find{|pair| pair.old_jar == oj}
+    pair = JarPair.new
+    pair.old_jar = oj
+    pairs << pair
   end
 }
 
-if options[:classes] || options[:source_changes]
-  `mkdir -p cache`
-  diff_jars_map.each{|nj,oj|
-    nj.cache_resource('cache', {:skip_signature_verfication => true, :verbose => true})
-    if oj
-      oj.cache_resource('cache', {:skip_signature_verfication => true, :verbose => true})
-    end
+diff_versions = pairs.select{|pair| pair.diff_versions?}
+
+`mkdir -p unzipped/old`
+`mkdir -p unzipped/new`
+`mkdir -p cache`
+
+# prefetching like this makes it clear that there are 2 phases of the compairison
+if $options[:classes] || $options[:source_changes]
+  diff_versions.each{|pair|
+    pair.new_jar.cache_resource('cache', {:skip_signature_verfication => true, :verbose => true})
+    pair.old_jar.cache_resource('cache', {:skip_signature_verfication => true, :verbose => true})
   }
 end
 
-if options[:classes]
-  `mkdir -p unzipped/old`
-  `mkdir -p unzipped/new`
-
-  content_differences = {}
-  diff_content_jars_map = {}
-  diff_jars_map.each{|nj,oj|
-    if oj.nil?
-      diff_content_jars_map[nj] = nil
-      next
-    end
-
-    `rm -rf unzipped/old/*`
-    `rm -rf unzipped/new/*`
-    `unzip -d unzipped/old #{oj.local_path} -x META-INF/*`
-    `unzip -d unzipped/new #{nj.local_path} -x META-INF/*`
-    differences = `cd unzipped; diff -r old new`
-    if $?.to_i == 0
-      # the class files match in these jars
-    else
-      diff_content_jars_map[nj] = oj
-      content_differences[nj] = differences
-    end
-  }
-
-  puts "The follow jars have different versions but their classes are identical"
-  diff_jars_map.each{|nj,oj|
-    next if oj.nil?
-    puts "  #{nj.href}" if !diff_content_jars_map[nj]
-  }
-
-  diff_jars = diff_content_jars_map
-end
-
-sizes = {}
-if options[:sizes]
-  total = 0
-  diff_jars_map.each{|nj,oj|
-  	begin
-  		url = URI.parse(nj.url_pack_gz)
-  		connection = Net::HTTP.new(url.host, url.port)
-  		response = connection.request_head(url.path)
-  		sizes[nj] = response.content_length
-  		total += response.content_length
-  	rescue
-  		puts "failed to get jar #{url.path.to_s}"
-  	end
-  }
-  puts "Total update size: #{total} bytes"
-end
-
-source_changes_map = {}
-if options[:source_changes]
-  `mkdir -p tmp`
-
-  diff_jars_map.each{|nj,oj|
-    if oj.nil?
-      source_changes_map[nj] = "no point in checking"
-      next
-    end
-
-    `rm -f tmp/*`
-    `unzip -j -d tmp #{nj.local_path} *pom.xml`
-    `cd tmp; mvn help:effective-pom -Doutput=effective-pom.xml`
-    # pull out the project/scm/connection url from the effective-pom.xml
-    scm_connection = `xpath tmp/effective-pom.xml "/project/scm/connection/text()"  2> /dev/null`
-    if scm_connection =~ /scm:svn:(.*)/
-      svn_info = `svn info #{$1}`
-      source_changes_map[nj] = svn_info.scan(/Last Changed Date:(.*)/)[0][0]
-    end
-  }
-end
-
-
-puts "Differences"
-diff_jars_map.each{|nj,oj|
-  size = sizes[nj] ? "\t#{sizes[nj]} bytes" : ""
-  source_change = source_changes_map[nj] ? "\t#{source_changes_map[nj]}" : ""
-  if oj
-    puts "  updated  #{nj.href}#{size}\t#{nj.version_str}\t#{oj.version_str}#{source_change}"
-    if options[:show_content_diff] && differences = content_differences[nj]
-    	puts differences
-    end
-  else
-    puts "  new      #{nj.href}#{size}\t#{nj.version_str}"
+def print_summary(name, pairs_to_summarize)
+  puts name
+  pairs_to_summarize.each{|pair| pair.print_diff }
+  if $options[:sizes]
+    total_size = 0
+    pairs_to_summarize.each{|pair| total_size += pair.new_jar_size }
+    puts "Size of added or changed jars #{total_size} bytes"
   end
-}
+  count = pairs_to_summarize.select{|pair| pair.new_jar}.size
+  puts "Number of added or changed jars #{count}"
+end
 
-puts "Number of jars: #{diff_jars_map.size}"
+# need a list of pairs that includes removed, added, and changed jars
+based_on_version = pairs.select{|pair| pair.new_jar.nil? || pair.old_jar.nil? || pair.diff_versions?}
+print_summary("Differences based on version", based_on_version)
+
+if $options[:classes]
+  puts "('v' means that only the versions are different but the content is the same)"
+  puts ""
+  based_on_content = pairs.select{|pair| pair.new_jar.nil? || pair.old_jar.nil? || pair.diff_contents?}
+  print_summary("Differences based on contents", based_on_content)
+end
+
+
